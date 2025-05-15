2f91a2a81f4d20a3e946b9e162ec1af205a39616
Матфей Лисов Игоревич <lmatfey11@gmail.com>
2025-05-16 02:30:18 +0300

    Мир книг

--- project_bibl/main.cpp
+++ project_bibl/main.cpp
@@ -2,6 +2,15 @@
 #include <iostream>
 
 int main() {
+	setlocale(LC_ALL, "rus");
+
+	std::cout << "Карманный помощник 'Мир книг' библиотеки имени Чехова.";
+	
+	int Key = 0;
+	
+	do {
+		
+	} while (Key == 0);
 	
 	return 0;
 }
