de2ad50282f8f1fd072060d11812309db44c07bc
Матфей Лисов Игоревич <lmatfey11@gmail.com>
2025-05-16 03:25:48 +0300

    hin/in/up/exit

--- project_bibl/main.cpp
+++ project_bibl/main.cpp
@@ -1,21 +1,25 @@
 ﻿#include "class.h"
 #include <iostream>
 #include <string>
+#include <windows.h> // для system("cls")
 
 int main() {
-	setlocale(LC_ALL, "rus");
+	setlocale(LC_ALL, "rus");//
 
-	std::cout << "Карманный помощник 'Мир книг' библиотеки имени Чехова." << std::endl;
-	std::cout << "______________________________________________________" << std::endl;
-	std::cout << "Войти, как гость (hin) / Войти (in) / Зарегистрироваться (up) / Выйти (exit)" << std::endl;
+	std::string str1 = "Карманный помощник 'Мир книг' библиотеки имени Чехова.";
+	std::string str2 = "______________________________________________________";
 	std::string Key = "";
-	int key = 0;
+	int key = -1;
+
+	std::cout << str1 << std::endl << str2 << std::endl;
 	
 	do {
-		std::cout << "Напиши слово из скобкок: ";
-		
+		std::cout << "Войти, как гость (hin) / Войти (in) / Зарегистрироваться (up) / Выйти (exit)" << std::endl << "Напиши слово из скобкок: ";
 		std::cin >> Key;
-		
+
+		system("cls"); // очищает экран консоли на Windows
+		std::cout << str1 << std::endl << str2 << std::endl;
+
 		if (Key == "hin") {
 			key = 0;
 		}
@@ -33,21 +37,39 @@ int main() {
 		{
 		case (0):
 			std::cout << "Вы гость нашей библиотеки! Добро пожаловать!" << std::endl;
+
+			//////////////////////////////
+
+			system("cls"); // очищает экран консоли на Windows
+			std::cout << str1 << std::endl << str2 << std::endl;
 			break;
 		case (1):
 			std::cout << "Наш верный посититель! С возращением!" << std::endl;
+
+			//////////////////////////////
+
+			system("cls"); // очищает экран консоли на Windows
+			std::cout << str1 << std::endl << str2 << std::endl;
 			break;
 		case (2):
 			std::cout << "Станьте членом нашей семьи!" << std::endl;
+
+			//////////////////////////////
+
+			system("cls"); // очищает экран консоли на Windows
+			std::cout << str1 << std::endl << str2 << std::endl;
 			break;
 		case (3):
 			std::cout << "Завершение работы. Будем у Вас в кормане, если что)";
+
+			//////////////////////////////
 			break;
 		default:
+			system("cls"); // очищает экран консоли на Windows
+			std::cout << str1 << std::endl << str2 << std::endl;
 			std::cout << "Неверная команда. Просим Вас написать нужную Вам для работы команду." << std::endl;
 			break;
 		}
-
 	} while (Key != "exit");
 	
 	return 0;
